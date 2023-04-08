bfe1c381f3da31ed6e91e781ebc8df11d0874162
Cyril Thomas <cyrilkthomasest.1991@gmail.com>
2023-04-07 22:32:47 -0400

    javafx for employee/customer menus

---
+++ Mod10-GarmentFactory/src/main/java/garment/CasualPants.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package garment;
+
+/**
+ *
+ * @author cyril
+ */
+public class CasualPants implements Pants {
+    public void print() {
+        System.out.println("Garment Type: Casual pants");
+    }
+}
---
+++ Mod10-GarmentFactory/src/main/java/garment/Mod10GarmentFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package garment;
+import garment.*;
+
+/**
+ *
+ * @author cyril
+ */
+public class Mod10GarmentFactory {
+    public static void client(GarmentFactory factory) {
+        Top top = factory.createTop();
+        Pants pants = factory.createPants();
+        Shoes shoes = factory.createShoes();
+
+        // Do something with the garments
+        top.print();
+        pants.print();
+        shoes.print();
+    }
+
+    public static void main(String[] args) {
+        // Create garments for the professional variant
+        GarmentFactory factory1 = new ProfessionalGarmentFactory();
+        client(factory1);
+
+        // Create garments for the casual variant
+        GarmentFactory factory2 = new CasualGarmentFactory();
+        client(factory2);
+
+        // Create garments for the party variant
+        GarmentFactory factory3 = new PartyGarmentFactory();
+        client(factory3);
+    }
+ 
+}
+
